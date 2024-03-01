3c8797cc77825f2c6d29bf042ff67b1872e1ad82
Nathan viana <viananathan03@gmail.com>
2024-03-01 20:13:43 -0300

    Aula06 - Operações aritméticas

---
+++ aula06/aula06.js
@@ -0,0 +1,28 @@
+/* Operações Aritméticas
+
+'+' Adição / Concatenação (- / *)
+'**' Potenciação
+'%' Resto da divisão
+
+*/
+
+const num1 = 5;
+const num2 = 2;
+const num3 = 10;
+console.log((num1 + num2) * num3); // norma padrão da matematica. Colocando entre parêntese se torna prioridade aquilo no calculo.
+
+// Em caso de operadores aritméticos de mesmo peso EX: (num 1 * num2 / num3), a ordem se segue da esquerda para a direita.
+
+// incremento = ++
+// decremento = --
+
+const passo = 50;
+let contador = 0;
+contador += passo; // contador = contador + 50
+console.log(contador);
+
+// NaN - Not a number parseInt (inteiro), parseFloat (decimais), number
+const num4 = 10;
+const num5 = Number('Luiz');
+console.log (num1 + num2); 1
+console.log(typeof num2);
