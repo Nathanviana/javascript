792e2e99cd9cdee6a94d1a5b00f62a55d8e71690
Nathan viana <viananathan03@gmail.com>
2024-03-08 15:29:07 -0300

    Create aula 011

---
+++ aula11/aula11.js
@@ -0,0 +1,27 @@
+let num1 = 9;
+
+//como arredondar para baixo
+let num2 = Math.floor(num1);
+
+//como arredondar para cima
+let num3 = Math.ceil(num1);
+
+//round ultiliza a logica matematica para arredontar 
+let num4 =  Math.round(num1);
+
+console.log(num3);
+
+//mostrando o maio numero da sequencia
+console.log(Math.max(1,2,3,4,5,6,-8,13,15,-18,2000,7,0));
+
+//mostrando o menor numero da sequencia
+console.log(Math.min(1,2,3,4,5,6,-8,13,15,-18,2000,7,0));
+
+//aleatorizar qualquer numero 
+const dado = Math.round(Math.random() * (6 - 1) + 1);
+
+console.log(dado);
+
+//como descobri a raiz quadrada do numero
+console.log(num1 ** 0.5);
+
