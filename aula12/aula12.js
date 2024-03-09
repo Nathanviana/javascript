c8939228ce75cf798c2b965f4d54ef01263b2f93
Nathan viana <viananathan03@gmail.com>
2024-03-09 17:36:02 -0300

    aula 12 - arrays

---
+++ aula12/aula12.js
@@ -0,0 +1,23 @@
+const alunos = ['noser', 'lufe', 'dival'] // arrays
+
+console.log(alunos.slice());
+
+// alunos.push('nathan'); // Adiciona no fim
+// alunos.push('pão');
+
+
+// alunos [alunos.length] = 'manu'; // Adiciona no fim
+// alunos [alunos.length] = 'delta';
+// alunos [alunos.length] = 'luan';
+
+// alunos[0] = 'Eduardo'; // Altera
+// alunos [3] = 'manu'; // Adicionando
+
+// alunos.unshift('delta'); // Adiciona no começo
+// alunos.unshift('manu');
+
+// const removido = alunos.shift(); // remove um indice do começo 
+// console.log(alunos);
+
+// delete alunos [1]; deleta a informação presente em um índice do Array, mas o mantém a quantidade de índices do Array
+// console.log(alunos);
