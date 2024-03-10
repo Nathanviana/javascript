a5b12242166f81fc1380821db336a2eaf3ff8234
Nathan viana <viananathan03@gmail.com>
2024-03-10 20:53:44 -0300

    atividade de logica

---
+++ atividade/atividade01.js
@@ -0,0 +1,8 @@
+// Escreva  um  algoritmo que  o usuário  digite o  valor  da  base  e  da  altura  de  um triângulo para calcular a sua área.
+
+let num1 = prompt('Digite o valor para a base de um triangulo')
+let num2 = prompt('Digite o valor para a altura de um triangulo')
+
+let arTri = num1 * num2 / 2;
+
+alert(`A área do triângulo é: ${arTri}`);
