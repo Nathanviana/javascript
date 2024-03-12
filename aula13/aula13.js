57361213bfe90d2e5d106fb18de1176bd6d0bc57
Nathan viana <viananathan03@gmail.com>
2024-03-11 21:05:27 -0300

    atualização da aula 13

--- aula13/aula13.js
+++ aula13/aula13.js
@@ -1,3 +1,5 @@
+// Aula de Funções
+
 function saudacao (nome) {
     return `Bom dia ${nome}!`;
 }
@@ -6,8 +8,9 @@ const variavel = saudacao ('Luiz');
 console.log(variavel);
 
 
+/* foram determinados valores padrões para x e y caso não recebam nem um valor do usuário. */
 function soma (x = 1, y = 1) {
-const resultado = x + y;
+const resultado = x + y; // const criada para receber o resultado da adição.
 return resultado;
 }
 
@@ -23,7 +26,7 @@ console.log(raiz(16));
 console.log(raiz(25));
 
 //arrow fuction
-const raiz = n => n ** 0.5;
+const raiz2 = n => n ** 0.5;
 
 console.log(raiz(9));
 console.log(raiz(16));
