9850fc9ecc32db41c89300a89dd9fe73c4238a73
Nathan viana <viananathan03@gmail.com>
2024-03-14 18:05:36 -0300

    aula01 - logica de progrmação

---
+++ javascript - lógica de programação/aula01/aula01.js
@@ -0,0 +1,17 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+<menor que
+<= menor que ou igual a
+== igualdade (valor) **
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) **
+!== diferente estrito (valor e tipo)
+*/
+
+const comp1 = 10 > 5;
+console.log(comp);
+
+const comp2 = 10 >= 5;
+console.log(comp);
