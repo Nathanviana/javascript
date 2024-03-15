9267471de37a79c8e1d1922e906737758118a45e
Nathan viana <viananathan03@gmail.com>
2024-03-15 13:33:07 -0300

    aula 02 - Lógica de programação

---
+++ javascript - lógica de programação/aula02/aula02.js
@@ -0,0 +1,15 @@
+/* 
+Operadores Lógicos
+&& -> AND -> E -> Todas as expressões precisam  ser verdadeiras para retornar true
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+// const expressãoAnd = true && true && true && true;
+const expressãoOr = false || true;
+
+const usuario = "luiz";
+const senha = 123456;
+
+const vaiLogar = usuario === "luiz" && senha === 123456;
+console.log(vaiLogar);
+
