cf1946ec686a59acd52ae0dc23c87c3757a90ede
Nathan viana <viananathan03@gmail.com>
2024-04-02 20:51:41 -0300

    Create for-in.js

---
+++ javascript - lógica de programação/aula12/for-in.js
@@ -0,0 +1,17 @@
+// For in -> lê os índices ou chaves do objeto
+
+const cor = ['azul', 'verde', 'branco'];
+
+for (let i in cor) {
+    console.log(cor[i]);
+}
+
+// const pessoa = {
+//     nome: 'Nathan',
+//     sobrenome: 'Viana',
+//     idade: 20
+// };
+
+// for (let index in pessoa) {
+//     console.log(index, pessoa[index]);
+// }
