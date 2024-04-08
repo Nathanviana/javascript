ad74e68f47172efd38fc46fc8557088722ab1d02
Nathan viana <viananathan03@gmail.com>
2024-04-08 18:46:01 -0300

    Create atv05.js

---
+++ javascript - lógica de programação/exercicios/atv 05/atv05.js
@@ -0,0 +1,16 @@
+// function maior (num1, num2) {
+    
+//     if (num1 > num2) {
+//         return num1;
+//     } else {
+//         return num2;
+//     }
+    
+// };
+
+// console.log(maior(100, 20));
+
+
+// Forma de reduzir o codigo
+const maior = (x, y) => x > y ? x : y;
+console.log(maior(100, 20));
