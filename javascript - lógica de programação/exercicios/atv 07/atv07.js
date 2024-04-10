758e066f1bf44d3ccadc2fefa2d9d034e66b1abd
Nathan viana <viananathan03@gmail.com>
2024-04-10 19:53:04 -0300

    Create atv07.js

---
+++ javascript - lógica de programação/exercicios/atv 07/atv07.js
@@ -0,0 +1,13 @@
+function ePaisagem (l, a) {
+    if (l > a) {
+        return 'true'
+    } else {
+        return 'false'
+    }
+}
+
+console.log(ePaisagem2(1960, 500));
+
+function ePaisagem2 (largura, altura) {
+    return largura > altura
+}
