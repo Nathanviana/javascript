534811796331890cc4b8cfd9e2243f156f2f1f8b
Nathan viana <viananathan03@gmail.com>
2024-04-11 20:16:10 -0300

    Update atv06.js

--- javascript - lógica de programação/exercicios/atv 06/atv06.js
+++ javascript - lógica de programação/exercicios/atv 06/atv06.js
@@ -8,9 +8,10 @@ function recebe(num) {
         return 'Fizz'
     } else if (num % 5 == 0) {
         return 'Buzz'
-    } else if (num % 3 !== 0 && num % 5 !== 0) {
+    } else { 
         return num
     }
+    
 
 }
 
